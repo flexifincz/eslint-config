938a1647d303ef46bb5bd602f95fe659019da897
Ondřej Misák <email@ondrejmisak.cz>
2025-05-20 11:41:01 +0200

    feat: update major deps

--- src/prettier.ts
+++ src/prettier.ts
@@ -1,7 +1,9 @@
+import type { Options } from 'prettier';
+
 export default function flexifinPreset() {
   return {
     printWidth: 100,
     singleQuote: true,
     trailingComma: 'es5',
-  };
+  } as Options;
 }
