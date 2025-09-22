670fa3c231d48b07a00860dec418cc86a4308e42
Ondřej Misák <email@ondrejmisak.cz>
2025-09-22 16:29:22 +0200

    chore(): update deps and rules (#8)

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,3 @@
-import flexiFin from './src/eslint.ts';
+import flexiFin from './src/eslint';
 
-export default flexiFin({ ignores: ['**/eslint.gen.d.ts'] }, { rules: {} });
+export default flexiFin({ ignores: ['**/types.gen.d.ts'] }, { rules: {} });
